e5d55553cac89913835e5a0ca1123beb6f30fea7
rockSuren <49206345+rockSuren@users.noreply.github.com>
2020-10-03 18:14:56 +0530

    Alignment and verbiage changes

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -17,7 +17,7 @@ export class AppointmentListComponent implements OnInit {
       rqst_id: 'Doctor Visit',
       reason: 'Update the task with initial doctor appointment date.',
       rqst_raised_by: "Ravi4567",
-      date: "Sep 27 2020",
+      date: "Oct 15 2020",
       time : '11 AM',
       status : 'Pending',
       comment : "Need to consult doctor"
@@ -25,29 +25,29 @@ export class AppointmentListComponent implements OnInit {
     {
       rqst_id: 'Doctor Visit',
       reason: 'Update the task with initial doctor appointment date.',
-      rqst_raised_by: "Ravi4567",
-      date: "Sep 27 2020",
-      time : '11 AM',
+      rqst_raised_by: "Logesh",
+      date: "Oct 17 2020",
+      time : '09 AM',
       status : 'Pending',
-      comment : "Need to consult doctor"
+      comment : "Need to discuss about breat cancer."
     },
     {
       rqst_id: 'Doctor Visit',
       reason: 'Update the task with initial doctor appointment date.',
-      rqst_raised_by: "Ravi4567",
-      date: "Sep 27 2020",
-      time : '11 AM',
+      rqst_raised_by: "Priya@543",
+      date: "Oct 27 2020",
+      time : '02 PM',
       status : 'Pending',
-      comment : "Need to consult doctor"
+      comment : "Have to know symptoms of breat cancer."
     },
     {
       rqst_id: 'Doctor Visit',
       reason: 'Update the task with initial doctor appointment date.',
-      rqst_raised_by: "Ravi4567",
-      date: "Sep 27 2020",
-      time : '11 AM',
+      rqst_raised_by: "Lavanya",
+      date: "Oct 09 2020",
+      time : '4 PM',
       status : 'Pending',
-      comment : "Need to consult doctor"
+      comment : "Having sypmtoms of breast cancer and need to know type of test taken."
     },
   ]
   }
